64a6975f2c58237b533045b78c2ec8c0b5f5e69d
Bulat <bulatyula@mail.ru>
2019-02-08 22:39:56 +0300

    Это наш второй коммит

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+
+console.log(leftBorderWidth);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//console.log(arr[2]);
+
+alert("Hello World!");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+//let answer = prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof(null));
+
+//console.log("arr" + "-  object");
+
+//console.log(typeof(4 + "-  object"));
+
+//let incr=10, decr=10;
+
+//incr++;
+//decr--;
+
+//console.log(incr);
+//console.log(decr);
+
+//console.log(5%2);
+
+let isChecked = false, isClose=true;
+
+console.log(isChecked || !isClose);
